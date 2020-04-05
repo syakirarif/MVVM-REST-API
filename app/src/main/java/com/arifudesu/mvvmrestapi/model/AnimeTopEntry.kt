02dd7dba99f3e539c404663ed2ea48840e663e15
syakir <muhsyakirarif12@gmail.com>
2020-04-05 20:18:54 +0700

    WIP

---
+++ app/src/main/java/com/arifudesu/mvvmrestapi/model/AnimeTopEntry.kt
@@ -0,0 +1,58 @@
+package com.arifudesu.mvvmrestapi.model
+
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "tb_anime_top")
+data class AnimeTopEntry(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+
+    @ColumnInfo(name = "malId")
+    @SerializedName("mal_id")
+    val malId: Int? = 0,
+
+    @ColumnInfo(name = "rank")
+    @SerializedName("rank")
+    val rank: Int? = 0,
+
+    @ColumnInfo(name = "title")
+    @SerializedName("title")
+    val title: String? = "",
+
+    @ColumnInfo(name = "url")
+    @SerializedName("url")
+    val url: String? = "",
+
+    @ColumnInfo(name = "imageUrl")
+    @SerializedName("image_url")
+    val imageUrl: String? = "",
+
+    @ColumnInfo(name = "type")
+    @SerializedName("type")
+    val type: String? = "",
+
+    @ColumnInfo(name = "episodes")
+    @SerializedName("episodes")
+    val episodes: Int? = 0,
+
+    @ColumnInfo(name = "startDate")
+    @SerializedName("start_date")
+    val startDate: String? = "",
+
+    @ColumnInfo(name = "endDate")
+    @SerializedName("end_date")
+    val endDate: String? = "",
+
+    @ColumnInfo(name = "members")
+    @SerializedName("members")
+    val members: Int? = 0,
+
+    @ColumnInfo(name = "score")
+    @SerializedName("score")
+    val score: Double? = 0.0
+)
